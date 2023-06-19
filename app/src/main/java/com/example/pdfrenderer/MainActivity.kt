67f2686c515d95bfb71b2c78331fae9d5fbd5cb0
jasonsuto <jason.suto@gmail.com>
2023-06-19 11:56:42 -0400

    updated README.md

--- app/src/main/java/com/example/pdfrenderer/MainActivity.kt
+++ app/src/main/java/com/example/pdfrenderer/MainActivity.kt
@@ -22,6 +22,8 @@ import java.net.URL
 class MainActivity : AppCompatActivity() {
     private lateinit var pdfImageView: ImageView
     private lateinit var webView: WebView
+    private val tempFile="temp.pdf"
+    private var  allowCaching=false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,18 +31,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         pdfImageView = findViewById(R.id.pdfImageView)
         webView = findViewById(R.id.webView)
+        //val gzUrl = "https://www.jumpskunk.com/adobe8.gz"
+        val gzUrl = "https://www.jumpskunk.com/ins.gz"
 
         GlobalScope.launch(Dispatchers.IO) {
-             //val gzUrl = "https://www.jumpskunk.com/test.pdf"
-            //val gzUrl = "https://www.jumpskunk.com/adobe.pdf"
-            //val gzUrl = "https://www.jumpskunk.com/adobe-zipped.gz"
-            val gzUrl = "https://www.jumpskunk.com/test3.gz"
+
             var  pdfFile = downloadInsuranceFile(gzUrl)
-
-            val bitmap = renderPdfPage(pdfFile)
-            val base64Image: String = bitmapToBase64(bitmap)
-            displayBitmapInWebView(base64Image)
-
+            val bitmaps = renderMultiplePdfPages(pdfFile)
+            val base64Images  = bitmapsToBase64(bitmaps)
+            displayBitmapsInWebView(base64Images)
 
         }
 
@@ -48,8 +47,10 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun downloadInsuranceFile(gzUrl: String): File {
-        val downloadedFileName = "temp3.pdf"
-        val file = File(cacheDir, downloadedFileName)
+       // val downloadedFileName = "temp3.pdf"
+        val file = File(cacheDir, tempFile)
+
+       if(!allowCaching&& file.exists()) file.delete()
 
         try {
             val url = URL(gzUrl)
@@ -67,57 +68,87 @@ class MainActivity : AppCompatActivity() {
             outputStream.close()
             inputStream.close()
             val fileSize = file.length()
+
         } catch (e: IOException) {
             e.printStackTrace()
         }
         return file
     }
-    private fun renderPdfPage(pdfFile: File): Bitmap? {
+
+    private fun renderMultiplePdfPages(pdfFile: File): List<Bitmap>? {
         var bitmap: Bitmap? = null
+        val bitmapList = mutableListOf<Bitmap>()
         try {
             val fileDescriptor: ParcelFileDescriptor = ParcelFileDescriptor.open(pdfFile, ParcelFileDescriptor.MODE_READ_ONLY)
             val pdfRenderer = PdfRenderer(fileDescriptor)
-            val page: PdfRenderer.Page = pdfRenderer.openPage(0)
-            val width: Int = page.width*3;           val height: Int=page.height*3
-            bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-            page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
 
-            page.close()
+            for (i in 1..pdfRenderer.pageCount) {
+                //todo: get all pages, return LISTOF bitmaps
+                val page: PdfRenderer.Page = pdfRenderer.openPage(i-1)
+                //The multiplyer determines final image resolution. "1" is too blurry when zoomed, 10 creates a huge image. 3 seems good.
+                val resolutionMult=3
+                val width: Int = page.width*resolutionMult;           val height: Int=page.height*resolutionMult
+                bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+                page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+
+                bitmapList.add(bitmap)
+                page.close()
+
+            }
             pdfRenderer.close()
             fileDescriptor.close()
         } catch (e: IOException) {
             e.printStackTrace()
         }
-        pdfFile.delete()
-        return bitmap
-    }
-    private suspend fun displayBitmapInWebView(base64Image: String) {
-        withContext(Dispatchers.Main) {
-            if (base64Image != null) {
-                val finalWidth: Int=(resources.displayMetrics.widthPixels/resources.displayMetrics.density).toInt()
-                //Log.e("metrics","wp:"+resources.displayMetrics.widthPixels+"  hp:"+ resources.displayMetrics.heightPixels + "    dp:"+resources.displayMetrics.densityDpi+"   d:"+resources.displayMetrics.density)
 
-                val htmlContent = """
+        return bitmapList
+    }
+
+
+    private fun bitmapsToBase64(bitmapList: List<Bitmap>?): List<String> {
+        val stringList=  mutableListOf<String>()
+
+        if (bitmapList != null) {
+            for (bitmap in bitmapList) {
+                val byteArrayOutputStream = ByteArrayOutputStream()
+                if (bitmap != null) {
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 60, byteArrayOutputStream)
+                    bitmap.recycle()
+                }
+                val byteArray: ByteArray = byteArrayOutputStream.toByteArray()
+                stringList.add(Base64.encodeToString(byteArray, Base64.DEFAULT))
+            }
+        }
+        return stringList
+    }
+
+    private suspend fun displayBitmapsInWebView(base64ImageList: List<String>) {
+        withContext(Dispatchers.Main) {
+            if (base64ImageList != null) {
+                val finalWidth: Int=(resources.displayMetrics.widthPixels/resources.displayMetrics.density).toInt()
+                //Can use stringbuilder if you wish
+                var htmlMiddle: String=""
+                for (imageString in base64ImageList) {
+                    htmlMiddle+= """
+                    <img src="data:image/png;base64,$imageString" alt="Bitmap Image" width="$finalWidth" ><br>
+                    """.trimIndent()
+                }
+                val htmlContent=     """
                     <html>
-                    <body>
-                    <img src="data:image/png;base64,$base64Image" alt="Bitmap Image" width="$finalWidth" >
-                    </body>
+                    <body>$htmlMiddle</body>
                     </html>
-                """.trimIndent()
+                   """.trimIndent()
+
 
                 webView.loadDataWithBaseURL(null, htmlContent, "text/html", "UTF-8", null)
                 webView.getSettings().setLoadWithOverviewMode(true);
             }
         }
     }
-    private fun bitmapToBase64(bitmap: Bitmap?): String {
-        val byteArrayOutputStream = ByteArrayOutputStream()
-        if (bitmap != null) {
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
-        }
-        val byteArray: ByteArray = byteArrayOutputStream.toByteArray()
-        return Base64.encodeToString(byteArray, Base64.DEFAULT)
-    }
+
+
+
+
 
 
 }
